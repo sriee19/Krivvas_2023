c755020e4250d51b0a241356b1cee9ccbbe4a049
vigneshwaranBS <bs.vigneshwaran@gmail.com>
2023-03-20 15:53:56 +0530

    comming soon updates

--- comingsoon/assets/js/jquery.knob.js
+++ comingsoon/assets/js/jquery.knob.js
@@ -1,14 +1,4 @@
-/**
- * Downward compatible, touchable dial
- *
- * Version: 1.2.11
- * Requires: jQuery v1.7+
- *
- * Copyright (c) 2012 Anthony Terrien
- * Under MIT License (http://www.opensource.org/licenses/mit-license.php)
- *
- * Thanks to vor, eskimoblood, spiffistan, FabrizioC
- */
+
 "use strict";
 (function(e) {
     if (typeof exports === "object") {
